83748c37a40614a32bc41c01900fe5867b0c5f90
Finlay Cannon <fin@finlaycannon.com>
2012-09-30 21:05:00 -0700

    Sorted list

--- linkedlist.py
+++ linkedlist.py
@@ -3,25 +3,31 @@
 import sys 
 
 class Node:
-    value = None
-    next = None
-
-    def __init__(self, value=None):
+    def __init__(self, value=None, next=None):
         self.value = value
+        self.next = next
 
-class List:
-    head = None
-    tail = None
+    def __str__(self):
+        return "[Node value=" + str(self.value) + "]"
 
+
+class SortedList:
     def __init__(self):
-        self.head = Node()
-        self.tail = self.head
+        self.head = None
 
     def insert(self, value):
-        node = Node(value)
-        self.tail.next = node
-        self.tail = node
+        prev = None
+        current = self.head
+        while current is not None and current.value <= value:
+            prev = current
+            current = current.next
 
+        new_node = Node(value, current)
+        if prev is None:
+            self.head = new_node
+        else:
+            prev.next = new_node
+    
     def find(self, value):
         node = self.head
         while (node is not None):
@@ -32,24 +38,28 @@ class List:
 
     def __str__(self):
         string = ""
-        node = self.head.next
+        node = self.head
         while (node is not None):
-            string += str(node.value) + ", "
+            string += str(node.value)
+            if node.next is not None:
+                string += ", "
             node = node.next
         return string
 
+
 def main(argv=None):
-    list = List()
+    list = SortedList()
     
+    list.insert(5)
     list.insert(2)
     list.insert(7)
     list.insert(9)
-    list.insert(3)
     list.insert(4)
+    list.insert(3)
 
     print list
-    print list.find(17)
-
+    print "find 6:", list.find(6)
+    print "find 7:", list.find(7)
 
 if __name__ == "__main__":
     sys.exit(main())
