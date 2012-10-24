3f59daeedd4a0d0783de3403f635a37760e8b30a
Finlay Cannon <fin@finlaycannon.com>
2012-10-23 21:18:42 -0700

    Fixed to only count all anagrams of a word once.

--- wooords/staaats.py
+++ wooords/staaats.py
@@ -5,40 +5,37 @@ Calculate which words are most common in a 9-letter Wooords game.
 """
 
 import sys
+from collections import defaultdict
 
-ANAGRAMS = {}
-MASK = [0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x100]
+ANAGRAMS = defaultdict(list)
+BIT = [0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x100]
+bits = range(9)
+combos = range(512)
 
 def load_dictionary(fname):
     f = open(fname)
     for word in f.readlines():
         word = word.rstrip("\n")
         key = "".join(sorted(word))
-        if not ANAGRAMS.has_key(key):
-            ANAGRAMS[key] = []
         ANAGRAMS[key].append(word)
     f.close()
 
-def find_anagrams(letters):
-    key = "".join(sorted(letters))
-    if ANAGRAMS.has_key(key):
-        return ANAGRAMS[key][:] # return a copy
-    return []
-
 def find_words(letters):
     words = []
-    for combo in range(512):
+    for combo in combos:
         subset = ""
-        for i in range(9):
-            if combo & MASK[i]:
+        for i in bits:
+            if combo & BIT[i]:
                 subset += letters[i]
-        if len(subset) > 3:
-            words.extend(find_anagrams(subset))
+
+        if len(subset) > 3 and subset in ANAGRAMS:
+            # don't need to sort because we preserved the order of letters 
+            #words.extend(ANAGRAMS[subset])
+            words.append(subset)
     return clean(words)
 
 def clean(words):
     words = list(set(words)) # de-dupe
-    words.sort()
     return words
 
 
@@ -46,17 +43,15 @@ def main():
     load_dictionary("./scrabble.txt")
 
     bingos = filter(lambda w: len(w) == 9, ANAGRAMS.keys())
-    word_counts = {}
+    word_counts = defaultdict(int)
 
     for letters in bingos:
         for word in find_words(letters):
             key = "".join(sorted(word))
-            if not word_counts.has_key(key):
-                word_counts[key] = 0
             word_counts[key] += 1
 
     top = sorted(word_counts, key=word_counts.get, reverse=True)[:100]
-
+    
     rk = 1
     for w in top:
         print str(rk) + ".",
