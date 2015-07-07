6301df5c236db8bb4fb9120754ecb503cace0555
Finlay Cannon <fin@finlaycannon.com>
2015-07-07 13:30:21 +0800

    Use defaultdict

--- pairs.py
+++ pairs.py
@@ -4,31 +4,28 @@ How many perfect anagrams are there? (only two words which anagram to each other
 """
 
 import sys
+from collections import defaultdict
 
-ANAGRAMS = {}
+ANAGRAMS = defaultdict(list)
 
 def load_dictionary(fname):
     f = open(fname)
     for word in f.readlines():
         word = word.rstrip("\n")
         key = "".join(sorted(word))
-        if not ANAGRAMS.has_key(key):
-            ANAGRAMS[key] = []
         ANAGRAMS[key].append(word)
     f.close()
 
 def main():
     load_dictionary("./dictionary.txt")
 
-    words = [entry[1] for entry in ANAGRAMS.items() if len(entry[1]) == 2] 
+    words = [entry[1] for entry in ANAGRAMS.items() if len(entry[1]) == 2]
     words = sorted(words, key=lambda pair: pair[0])
 
     for pair in words:
         print ", ".join(pair)
-
     print
     print "Total:", len(words)
 
 if __name__ == '__main__':
     main()
-
